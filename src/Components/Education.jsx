f55423702b14fb7c2f86f1509f1546f468bcbca8
tommydebisi <tommydebisi@gmail.com>
2023-01-08 15:21:09 +0100

    :construction: education section created

---
+++ src/Components/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section id="education" className="light">
+      <h1>Education</h1>
+      <div className="content">
+        <div className="item">
+          <h2>University of Lagos</h2>
+          <h3>2018 - 2024</h3>
+          <p>Bachelor of Science in Surveying & Geo-informatics Engineering</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Education;
